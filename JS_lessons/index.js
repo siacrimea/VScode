47e8bb86a9c1a8ce2463ae45bad9852dcbd42dd9
siacrimea <siacrimea@gmail>
2021-04-30 15:51:17 +0300

    30 апреля

--- JS_lessons/index.js
+++ JS_lessons/index.js
@@ -1,9 +1,129 @@
-let arr = ["HTML", "JavaScript", "CSS"];
+function unique(arr) {
+    let uni = [];
+    arr.forEach(function(item) {
+        if (!uni.includes(item)) {
+            uni.push(item);
+        }
+    });
+    return uni;
+}
 
-let sorted = arr.slice().sort();
+let strings = [
+    "кришна",
+    "кришна",
+    "харе",
+    "харе",
+    "харе",
+    "харе",
+    "кришна",
+    "кришна",
+    ":-O",
+];
 
-alert(sorted); // CSS, HTML, JavaScript
-alert(arr); //HTML, JavaScript, CSS (без изменений)
+alert(strings.includes("кришна"));
+
+alert(unique(strings));
+
+// function getAverageAge(users) {
+//     return users.reduce((sum, current) => sum + current.age, 0) / users.length;
+// }
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 29 };
+
+// let arr = [vasya, petya, masha];
+
+// alert(getAverageAge(arr));
+
+// function sortByAge(arr) {
+//     arr = arr.sort((a, b) => (a.age > b.age ? 1 : -1));
+//     }
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let arr = [vasya, petya, masha];
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// alert(arr[0].name); // Вася
+// alert(arr[1].name); // Маша
+// alert(arr[2].name); // Петя
+
+//-----------------------------------
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [vasya, petya, masha];
+
+// let usersMapped = users.map((elem) => ({
+//     fullName: elem.name + " " + elem.surname,
+//     id: elem.id,
+
+// }));
+
+// alert(usersMapped[0].id); // 1
+// alert(usersMapped[0].fullName); // Вася Пупкин
+// alert(usersMapped[1].id); // 1
+// alert(usersMapped[1].fullName);
+//----------------------------------------
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [vasya, petya, masha];
+
+// let names = users.map((item) => item.name);
+
+// alert(names); // Вася, Петя, Маша
+
+//-----------------------------------------
+
+// function Calculator() {
+//     this.methods = {
+//         "+": function(a, b) {
+//             return a + b;
+//         },
+//         "-": function(a, b) {
+//             return a + b;
+//         },
+//     };
+
+//     this.calculate = function(str) {
+//         let split = str.split(" "),
+//             a = +split[0],
+//             op = split[1],
+//             b = +split[2];
+
+//         return this.methods[op](a, b);
+//     };
+
+//     this.addMethod = function(name, func) {
+//         this.methods[name] = func;
+//     };
+// }
+// let powerCalc = new Calculator();
+
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+
+// alert(powerCalc.calculate("3 ** 7"));
+
+////----------------------------------------------
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// let sorted = arr.slice().sort();
+
+// alert(sorted); // CSS, HTML, JavaScript
+// alert(arr); //HTML, JavaScript, CSS (без изменений)
 
 // let arr = [5, 2, 1, -10, 8];
 
