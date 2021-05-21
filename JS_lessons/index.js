1d79657254fedc751d65a2214cb7a8d51d1bd357
siacrimea <siacrimea@gmail>
2021-05-21 16:49:19 +0300

    21

--- JS_lessons/index.js
+++ JS_lessons/index.js
@@ -1,28 +1,259 @@
-function unique(arr) {
-    let uni = [];
-    arr.forEach(function(item) {
-        if (!uni.includes(item)) {
-            uni.push(item);
-        }
-    });
-    return uni;
-}
+// function getSecondsToday() {
+//     let date = new Date();
+//     date = date.setHours(0, 0, 0, 0);
+//     return((Date.now() - date) / 1000);
+// }
 
-let strings = [
-    "кришна",
-    "кришна",
-    "харе",
-    "харе",
-    "харе",
-    "харе",
-    "кришна",
-    "кришна",
-    ":-O",
-];
+alert(getSecondsToday());
 
-alert(strings.includes("кришна"));
+// function getDateAgo(dat, days) {
+//     alert(dat);
+//     alert(dat.setDate(dat.getDate() - days));
+//     return new Date(dat.setDate(dat.getDate() - days));
 
-alert(unique(strings));
+//     //return new Date(dat.getTime() - days * 24 * 3600 * 1000);
+//     //alert(newdate);
+// }
+
+// let date = new Date(2015, 0, 2);
+
+// alert(getDateAgo(date, 1)); // 1, (1 Jan 2015)
+// alert(date);
+// alert(getDateAgo(date, 2)); // 31, (31 Dec 2014)
+// alert(getDateAgo(date, 365)); // 2, (2 Jan 2014)
+
+// function getWeekDay(date) {
+//     let days = ["ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ"];
+
+//     return days[date.getDay()];
+
+//     // let numday = date.getDay();
+//     // switch (numday) {
+//     //     case 0:
+//     //         return "ВС";
+//     //         break;
+//     //     case 1:
+//     //         return "ПН";
+//     //         break;
+//     //     case 2:
+//     //         return "ВТ";
+//     //         break;
+//     //     case 3:
+//     //         return "СР";
+//     //         break;
+//     //     case 4:
+//     //         return "ЧТ";
+//     //         break;
+//     //     case 5:
+//     //         return "ПТ";
+//     //         break;
+//     //     case 6:
+//     //         return "СБ";
+//     //         break;
+//     // }
+// }
+
+// let date = new Date(2012, 0, 3); // 3 января 2012 года
+// //alert(date.getDay()); // нужно вывести "ВТ"
+
+// alert(getWeekDay(date)); // нужно вывести "ВТ"
+
+// let dat = new Date(2012, 02, 20 03, 12);
+
+// alert(dat);
+
+// function diffSubtract(date1, date2) {
+//     return date2 - date1;
+// }
+
+// function diffGetTime(date1, date2) {
+//     return date2.getTime() - date1.getTime();
+// }
+
+// function bench(f) {
+//     let date1 = new Date(0);
+//     let date2 = new Date();
+
+//     let start = Date.now();
+//     for (let i = 0; i < 100000; i++) f(date1, date2);
+//     return Date.now() - start;
+// }
+
+// alert("Время diffSubtract: " + bench(diffSubtract) + "мс");
+// alert("Время diffGetTime: " + bench(diffGetTime) + "мс");
+
+// function topSalary() {
+//     let max = 0;
+//     let maxName = null;
+
+//     for (let [name, salary] of Object.entries(salaries)) {
+//         if (max < salary) {
+//             max = salary;
+//             maxName = name;
+//         }
+//     }
+
+//     return maxName;
+// }
+
+// let salaries = {
+//     John: 100,
+//     Pete: 300,
+//     Mary: 250,
+// };
+
+// alert(topSalary(salaries));
+
+// let user = { name: "John", years: 30 };
+
+// let { name, years: age, isAdmin = false } = user;
+
+// // ваш код должен быть с левой стороны:
+// // ... = user
+
+// alert(name); // John
+// alert(age); // 30
+// alert(isAdmin); // false
+
+// let user = {};
+// [user.name, user.surname] = "Ilya Kantor".split(" ");
+
+// alert(user.name); // Ilya
+
+// function count(obj) {
+//     return Object.keys(obj).length;
+// }
+
+// let user = {
+//     name: "John",
+//     age: 30,
+// };
+
+// alert(count(user)); // 2
+
+// function sumSalaries() {
+//     let sum = 0;
+//     for (let value of Object.values(salaries)) {
+//         sum = sum + value;
+//     }
+//     return sum;
+// }
+
+// let salaries = {
+//     John: 100,
+//     Pete: 300,
+//     Mary: 250,
+// };
+
+// alert(sumSalaries(salaries));
+
+// // let messages = [
+// //     { text: "Hello", from: "John" },
+// //     { text: "How goes?", from: "John" },
+// //     { text: "See you soon", from: "Alice" },
+// // ];
+
+// let messages = { text: "Hello", from: "John" };
+
+// alert(Object.keys(messages));
+
+// let readMap = new WeakMap();
+
+// //readMap.set(messages[0], new Date(2017, 1, 1));
+
+// if (!readMap.has(messages[0])) {
+//     readMap.set(messages[0], new Date(2019, 1, 1));
+// }
+
+// alert(readMap.get(messages[0]));
+
+// let map = new Map();
+
+// map.set("name", "John");
+// console.log(map);
+// let keys = Array.from(map.keys());
+// console.log(keys);
+// // Error: keys.push is not a function
+// // Ошибка: keys.push -- это не функция
+// keys.push("more");
+
+// function aclean(arr) {
+//     let map = new Map();
+
+//     for (let word of arr) {
+//         // разбиваем слово на буквы, сортируем и объединяем снова в строку
+//         let sorted = word.toLowerCase().split("").sort().join(""); // (*)
+//         map.set(sorted, word);
+//     }
+//     console.log(map);
+//     return Array.from(map.values());
+// }
+
+// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+// alert(aclean(arr));
+
+// function unique(arr) {
+//     return Array.from(new Set(arr));
+//     // //console.log(set);
+//     // let rez = [];
+//     // for (let val of set) {
+//     //     rez.push(val);
+//     // }
+//     // return rez;
+// }
+
+// let values = [
+//     "Hare",
+//     "Krishna",
+//     "Hare",
+//     "Krishna",
+//     "Krishna",
+//     "Krishna",
+//     "Hare",
+//     "Hare",
+//     ":-O",
+// ];
+
+// alert(unique(values)); // Hare,Krishna,:-O
+
+// let arrayLike = {
+//     0: "Hello",
+//     1: "World",
+//     length: 2,
+// };
+
+// let arr = Array.from(arrayLike); // (*)
+// alert(arr);
+// alert(arr.pop()); // World (метод работает)
+// alert(arr.pop());
+// alert(arr);
+
+// function unique(arr) {
+//     let uni = [];
+//     arr.forEach(function(item) {
+//         if (!uni.includes(item)) {
+//             uni.push(item);
+//         }
+//     });
+//     return uni;
+// }
+
+// let strings = [
+//     "кришна",
+//     "кришна",
+//     "харе",
+//     "харе",
+//     "харе",
+//     "харе",
+//     "кришна",
+//     "кришна",
+//     ":-O",
+// ];
+
+// alert(strings.includes("кришна"));
+
+// alert(unique(strings));
 
 // function getAverageAge(users) {
 //     return users.reduce((sum, current) => sum + current.age, 0) / users.length;
